f481b8ab54574bf7826e4e5da2f629312cc33ebb
Parth <pparth91@gmail.com>
2019-06-25 13:16:15 +0530

    Addition of fonts

--- javascript/views/home_page.js
+++ javascript/views/home_page.js
@@ -1,17 +1,18 @@
 define([
-	'backbone'
-	], function(Backbone, Data, MessengerModel, TeamView, ChatView, ContactModal, main_tpl) {
+	'backbone',
+	'text!templates/home_page.html'
+	], function(Backbone, HomeTemplate) {
 	var HomePage = Backbone.View.extend({
 
 		el: '#body',
 
 		initialize: function(options) {
 			this.options = options;
-			debugger
+			this.render();
 		},
 
 		render: function () {
-			
+			this.$el.append(_.template(HomeTemplate)());
 		},
 		
 		destroy: function () {
